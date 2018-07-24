668932a5eaa5504ddf3b9c65e9646681f984c81c
uber <nikleft12@gmail.com>
2018-07-24 18:17:31 +0300

    Added progress for MediaPlayer

--- StepikApp1/app/src/main/java/com/example/uber/stepikapp1/Utils.kt
+++ StepikApp1/app/src/main/java/com/example/uber/stepikapp1/Utils.kt
@@ -1,10 +1,5 @@
 package com.example.uber.stepikapp1
 
-import android.app.Service
-import android.content.Intent
-import android.media.MediaPlayer
-import android.net.Uri
-import android.os.IBinder
 import io.reactivex.Observable
 import java.net.HttpURLConnection
 import java.net.URL
@@ -27,42 +22,3 @@ fun createRequest(url: String) =
 
     }
 
-class PlayService: Service() {
-
-
-    var player: MediaPlayer?=null
-
-    override fun onCreate() {
-        super.onCreate()
-    }
-
-    override fun onDestroy() {
-        player?.stop()
-        super.onDestroy()
-    }
-
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
-        player?.stop()
-
-        val url=intent!!.extras.getString("mp3")
-        player = MediaPlayer()
-        player?.setDataSource(this, Uri.parse(url))
-
-        player?.setOnPreparedListener{p ->
-            p.start()
-        }
-
-        player?.prepareAsync()
-
-        return START_NOT_STICKY
-    }
-
-
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
-    }
-
-
-}
